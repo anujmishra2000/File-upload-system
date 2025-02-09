ed61aa8513a86de286b036d17e543814df7608c1
Anuj Mishra <anuj.2000.mishra@gmail.com>
2025-02-10 00:47:03 +0530

    changes

--- app/controllers/file_uploads_controller.rb
+++ app/controllers/file_uploads_controller.rb
@@ -1,5 +1,5 @@
 class FileUploadsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :short_url_redirect
 
   def index
     @files = current_user.file_uploads
--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -3,6 +3,7 @@ class FileUpload < ApplicationRecord
   has_one_attached :file
 
   validates :file, presence: true
+  validate :file_size_validation
 
   def get_public_url
     return unless public_url.present?
@@ -15,5 +16,13 @@ class FileUpload < ApplicationRecord
       break random_key unless FileUpload.exists?(public_url: random_key)
     end
     self.update(public_url: key)
+  end
+
+  private
+
+  def file_size_validation
+    if file.attached? && file.blob.byte_size > 1.megabyte
+      errors.add(:base, "File size must be less than 1GB")
+    end
   end
 end
